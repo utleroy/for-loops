65e204b0c01558828ecf4ba75667adc70381cbc0
Bryan Hankins <bryanhankins@me.com>
2016-07-18 19:59:54 -0500

    decrement divisible by 2

--- for-loops.js
+++ for-loops.js
@@ -1,41 +1,19 @@
 
-var counter = 5;
-for (; counter<=120; counter+=10) {
-  var text = "current value is " + counter;
-  text;
+for (var counter = 5; counter<=120; counter+=10) {
 
-  console.log(text);
+  var output = "current value is " + counter;
+  output;
+
+  console.log(output);
 }
 
+for (var i = 4096; i >=1; i = i / 2) {
+var output = i;
+output;
 
-// Instructions
+  console.log(output);
+}
 
-// Increment by 10
-
-// Write a for loop that uses a counter variable initialized at 5 and then increments it by 10 every time it executes. Use console.log() to output the value of the counter variable each time through the loop. Stop execution of the loop if the counter variable's value is greater than 120.
-
-// Example output:
-
-// Current value is 5
-
-// Current value is 15
-
-// Current value is 25
-
-// ...
-// Decrement by division
-
-// Write a for loop with a counter variable initialized at 4096. Each time the loop executes divide the counter variable's value by 2. Use console.log() to output its value every time. When the counter variable's value is 1, stop execution.
-
-// Example output:
-
-// Current value is 4096
-
-// Current value is 2048
-
-// Current value is 1024
-
-// ...
 // Array iteration
 
 // Create an array that contains the names of American Presidents. Loop over that array with a for loop, and use string concatenation with console.log() to output the order and name of each President (see example below).
